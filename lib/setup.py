095e82bb80c89ee561bfa50dcd51ad868ef393a2
Samuel Trávníček <sam.travnicek@gmail.com>
2024-01-23 20:22:42 +0100

    Updated setup script.

--- lib/setup.py
+++ lib/setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.1.2'
+VERSION = '0.1.3'
 DESCRIPTION = 'Automation/Scripting library for CST Studio Suite'
 LONG_DESCRIPTION = 'A package that allows Automation/Scripting for CST Studio Suite. It provides wrappers around CST Studio Suite python library and some utilities.'
 
